66b13f7cf27ae5aaeda10c89597068760aab36af
Luuk Janssen <ljjanssen@ucdavis.edu>
2025-03-03 15:50:24 -0800

    intiale data exploration

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# read the data into dataframe
+# import data
 df = pd.read_csv('brca.csv')
 
-print(df)
+# Initial exploration
+print(df.shape)
+print(df.head())
+print(df.info())
+print(df.describe())
+
+print(df['y'].value_counts())
+
+# Remove unnecassary column
+df = df.drop(df.columns[0], axis=1)
+
